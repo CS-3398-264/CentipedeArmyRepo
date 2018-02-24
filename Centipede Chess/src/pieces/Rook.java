e0a92cff27280ac682e1815e406c5f6a9fbc974a
JedaHutto <35769951+JedaHutto@users.noreply.github.com>
2018-02-24 13:33:56 -0600

    Correct movement

--- Centipede Chess/src/pieces/Rook.java
+++ Centipede Chess/src/pieces/Rook.java
@@ -22,6 +22,66 @@ public class Rook extends Piece
 	@Override
 	public void updatePossibleMoves()
 	{
+		for(int i = this.posY + 1; i < 8; i++) 
+		{
+			if(!hasPieceOn(this.posX, i))
+			{
+				this.possibleMoves[this.posX][i] = true;
+			}
+			else
+			{
+				if(this.pieceColor != board.returnPiece(this.posX, i).pieceColor)
+				{
+					this.possibleMoves[this.posX][i] = true;
+				}
+				break;
+			}
+		}
+		for(int i = this.posY - 1; i >= 0; i--) 
+		{
+			if(!hasPieceOn(this.posX, i))
+			{
+				this.possibleMoves[this.posX][i] = true;
+			}
+			else
+			{
+				if(this.pieceColor != board.returnPiece(this.posX, i).pieceColor)
+				{
+					this.possibleMoves[this.posX][i] = true;
+				}
+				break;
+			}
+		}
 		
+		for(int i = this.posX + 1; i < 8; i++) 
+		{
+			if(!hasPieceOn(i, this.posX))
+			{
+				this.possibleMoves[i][this.posX] = true;
+			}
+			else
+			{
+				if(this.pieceColor != board.returnPiece(this.posX, i).pieceColor)
+				{
+					this.possibleMoves[i][this.posX] = true;
+				}
+				break;
+			}
+		}
+		for(int i = this.posX - 1; i >= 0; i--) 
+		{
+			if(!hasPieceOn(i, this.posX))
+			{
+				this.possibleMoves[this.posX][i] = true;
+			}
+			else
+			{
+				if(this.pieceColor != board.returnPiece(this.posX, i).pieceColor)
+				{
+					this.possibleMoves[this.posX][i] = true;
+				}
+				break;
+			}
+		}
 	}
 }
