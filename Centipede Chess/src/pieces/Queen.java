3cb6349cfe6775d17b74d686e8cc0aaa0296a3e2
w0205700 <rcw66@txstate.edu>
2018-03-02 13:15:56 -0600

    Update Queen.java

--- Centipede Chess/src/pieces/Queen.java
+++ Centipede Chess/src/pieces/Queen.java
@@ -21,6 +21,66 @@ public class Queen extends Piece
 	@Override
 	public void updatePossibleMoves()
 	{
+		for(int i = this.posY + 1; i < 8; i++) 
+		{
+			if(!hasPieceOn(this.posX, i))
+			{
+				this.possibleMoves[this.posX][i] = true;
+			}
+			else
+			{
+				if(this.pieceColor != board.returnPiece(this.posX, i).pieceColor)
+				{
+					this.possibleMoves[this.posX][i] = true;
+				}
+				break;
+			}
+		}
+		for(int i = this.posY - 1; i >= 0; i--) 
+		{
+			if(!hasPieceOn(this.posX, i))
+			{
+				this.possibleMoves[this.posX][i] = true;
+			}
+			else
+			{
+				if(this.pieceColor != board.returnPiece(this.posX, i).pieceColor)
+				{
+					this.possibleMoves[this.posX][i] = true;
+				}
+				break;
+			}
+		}
 		
+		for(int i = this.posX + 1; i < 8; i++) 
+		{
+			if(!hasPieceOn(i, this.posY))
+			{
+				this.possibleMoves[i][this.posY] = true;
+			}
+			else
+			{
+				if(this.pieceColor != board.returnPiece(i, this.posY).pieceColor)
+				{
+					this.possibleMoves[i][this.posY] = true;
+				}
+				break;
+			}
+		}
+		for(int i = this.posX - 1; i >= 0; i--) 
+		{
+			if(!hasPieceOn(i, this.posY))
+			{
+				this.possibleMoves[i][this.posY] = true;
+			}
+			else
+			{
+				if(this.pieceColor != board.returnPiece(i, this.posY).pieceColor)
+				{
+					this.possibleMoves[i][this.posY] = true;
+				}
+				break;
+			}
+		}
 	}
 }
