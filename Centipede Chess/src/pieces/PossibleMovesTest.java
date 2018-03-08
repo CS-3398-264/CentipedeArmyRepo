6be6f9e0a0663569e458482126fec02a52f4ec4e
JesseMiara <35813850+JesseMiara@users.noreply.github.com>
2018-03-07 22:50:09 -0600

    added asserts, Tests, and rid errors

--- Centipede Chess/src/pieces/PossibleMovesTest.java
+++ Centipede Chess/src/pieces/PossibleMovesTest.java
@@ -7,73 +7,80 @@ import org.junit.Test;
 public class PossibleMovesTest 
 {
 
+	@Test
   public void BishopPossibleMoves()
   {
-    Board board = new Board();
-    Bishop bishop  = new Bishop(board, 3, 4, white);
-    Pawn pawn1 = new Pawn(board, 0, 7, white);
-    Pawn pawn2 = new Pawn(board, 6, 7, black);
-    Pawn pawn3 = new Pawn(board, 0, 1, white);
-    Pawn pawn4 = new Pawn(board, 7, 0, black);
+    Board board = new Board(0, 0, 0);
+    Bishop bishop  = new Bishop(board, 3, 4, 1);
+    Pawn pawn1 = new Pawn(board, 0, 7, 1);
+    Pawn pawn2 = new Pawn(board, 6, 7, 0);
+    Pawn pawn3 = new Pawn(board, 0, 1, 1);
+    Pawn pawn4 = new Pawn(board, 7, 0, 0);
+    Assert.assertNotEquals(pawn1, bishop);
   }
-  
+  @Test
   public void KingPossibleMoves()
   {
-    Board board = new Board();
-    King king = new King(board, 3, 4, white);
-    Pawn pawn1 = new Pawn(board, 4, 4, black);
-    Pawn pawn2 = new Pawn(board, 2, 4, white);
-    Pawn pawn3 = new Pawn(board, 5, 2, black);
-    Pawn pawn4 = new Pawn(board, 5, 6, black);
-    Pawn pawn5 = new Pawn(board, 1, 2, white);
-    Pawn pawn6 = new Pawn(board, 1, 6, white);
-    Pawn pawn7 = new Pawn(board, 4, 2, black);
-    Pawn pawn8 = new Pawn(board, 2, 6 white);
+    Board board = new Board(0, 0, 0);
+    King king = new King(board, 3, 4, 1);
+    Pawn pawn1 = new Pawn(board, 4, 4, 0);
+    Pawn pawn2 = new Pawn(board, 2, 4, 1);
+    Pawn pawn3 = new Pawn(board, 5, 2, 0);
+    Pawn pawn4 = new Pawn(board, 5, 6, 0);
+    Pawn pawn5 = new Pawn(board, 1, 2, 1);
+    Pawn pawn6 = new Pawn(board, 1, 6, 1);
+    Pawn pawn7 = new Pawn(board, 4, 2, 0);
+    Pawn pawn8 = new Pawn(board, 2, 6, 1);
+    Assert.assertNotEquals(pawn1,king);
   }
-  
+  @Test
   public void KnightPossibleMoves()
   {
-    Board board = new Board();
-    Knight knight = new Knight(board, 3, 4, white);
-    Pawn pawn1 = new Pawn(board, 5, 3 white);
-    Pawn pawn2 = new Pawn(board, 5, 5, black);
-    Pawn pawn3 = new Pawn(board, 1, 3, white);
-    Pawn pawn4 = new Pawn(board, 1, 5, black);
-    Pawn pawn5 = new Pawn(board, 2, 2, white);
-    Pawn pawn6 = new Pawn(board, 4, 2, black);
-    Pawn pawn7 = new Pawn(board, 2, 6, white);
-    Pawn pawn8 = new Pawn(board, 4, 6, black);
+    Board board = new Board(0, 0, 0);
+    Knight knight = new Knight(board, 3, 4, 1);
+    Pawn pawn1 = new Pawn(board, 5, 3, 1);
+    Pawn pawn2 = new Pawn(board, 5, 5, 0);
+    Pawn pawn3 = new Pawn(board, 1, 3, 1);
+    Pawn pawn4 = new Pawn(board, 1, 5, 0);
+    Pawn pawn5 = new Pawn(board, 2, 2, 1);
+    Pawn pawn6 = new Pawn(board, 4, 2, 0);
+    Pawn pawn7 = new Pawn(board, 2, 6, 1);
+    Pawn pawn8 = new Pawn(board, 4, 6, 0);
+    Assert.assertNotEquals(pawn1, knight);
   }
-  
+  @Test
   public void PawnPossibleMoves()
   {
-    Board board = new Board();
-    Pawn pawn1 = new Pawn(board, 0, 4, white);
-    Pawn pawn2 = new Pawn(board, 1, 3, white);
-    Pawn pawn3 = new Pawn(board, 1, 5, black);
+    Board board = new Board(0, 0, 0);
+    Pawn pawn1 = new Pawn(board, 0, 4, 1);
+    Pawn pawn2 = new Pawn(board, 1, 3, 1);
+    Pawn pawn3 = new Pawn(board, 1, 5, 0);
+    Assert.assertNotEquals(pawn1, pawn2);
   }
-  
+  @Test
   public void QueenPossibleMoves()
   {
-    Board board = new Board();
-    Queen queen = new Queen(board, 3, 4, white);
-    Pawn pawn1 = new Pawn(board, 0, 7, white);
-    Pawn pawn2 = new Pawn(board, 6, 7, black);
-    Pawn pawn3 = new Pawn(board, 0, 1, white);
-    Pawn pawn4 = new Pawn(board, 7, 0, black);
-    Pawn pawn5 = new Pawn(board, 0, 4, white);
-    Pawn pawn6 = new Pawn(board, 7, 4, black);
-    Pawn pawn7 = new Pawn(board, 0, 0, white);
-    Pawn pawn8 = new Pawn(board, 0, 7, black);
+    Board board = new Board(0, 0, 0);
+    Queen queen = new Queen(board, 3, 4, 1);
+    Pawn pawn1 = new Pawn(board, 0, 7, 1);
+    Pawn pawn2 = new Pawn(board, 6, 7, 0);
+    Pawn pawn3 = new Pawn(board, 0, 1, 1);
+    Pawn pawn4 = new Pawn(board, 7, 0, 0);
+    Pawn pawn5 = new Pawn(board, 0, 4, 1);
+    Pawn pawn6 = new Pawn(board, 7, 4, 0);
+    Pawn pawn7 = new Pawn(board, 0, 0, 1);
+    Pawn pawn8 = new Pawn(board, 0, 7, 0);
+    Assert.assertNotEquals(pawn1, queen);
   }
-  
+  @Test
   public void RookPossibleMoves()
   {
-    Board board = new Board();
-    Rook rook = new Rook(board, 3, 4, white);
-    Pawn pawn1 = new Pawn(board, 0, 4, white);
-    Pawn pawn2 = new Pawn(board, 7, 4, black);
-    Pawn pawn3 = new Pawn(board, 0, 0, white);
-    Pawn pawn4 = new Pawn(board, 0, 7, black);
+    Board board = new Board(0, 0, 0);
+    Rook rook = new Rook(board, 3, 4, 1);
+    Pawn pawn1 = new Pawn(board, 0, 4, 1);
+    Pawn pawn2 = new Pawn(board, 7, 4, 0);
+    Pawn pawn3 = new Pawn(board, 0, 0, 1);
+    Pawn pawn4 = new Pawn(board, 0, 7, 0);
+    Assert.assertNotEquals(pawn1, rook);
   }
 }
