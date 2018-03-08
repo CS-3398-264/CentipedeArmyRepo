dc84afb43ff8c8c695c763a43584da7e7977bbea
JesseMiara <35813850+JesseMiara@users.noreply.github.com>
2018-03-07 22:47:16 -0600

    Add files via upload
    Empty JUnit test class for additional unit tests

---
+++ Centipede Chess/src/chessTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class chessTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	public void testInitialPieces() {
+	
+	}
+
+}
